7f72d3ded22437218608b4bbbafd562c40e89bd0
benKapl <benji1.kaplan@gmail.com>
2025-03-09 16:45:49 +0100

    created first serveur on 8080

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    "8080",
+	}
+
+	server.ListenAndServe()
+
+	defer server.Close()
+
+}
