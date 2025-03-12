a1e1e297d3cdb530ac441fb8475d06ab5e45c653
benKapl <benji1.kaplan@gmail.com>
2025-03-12 18:08:49 +0100

    added function to replace specific words

--- validate_chirp.go
+++ validate_chirp.go
@@ -3,16 +3,16 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-
 	type requestBody struct {
 		Body string `json:"body"`
 	}
 
 	type responseBody struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -23,14 +23,32 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if len(params.Body) > 140 {
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", err)
 		return
 	}
 
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
 	respondWithJSON(w, http.StatusOK, responseBody{
-		Valid: true,
+		CleanedBody: getCleanedBody(params.Body, badWords),
 	})
 	return
+}
 
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
 }
