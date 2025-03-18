318ca69d50a35ee492fd57a16f8d2a5e50e2d45d
benKapl <benji1.kaplan@gmail.com>
2025-03-18 10:46:47 +0100

    created hashpawssord func and first test

---
+++ internal/auth/hash.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	cost := 10
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return "", fmt.Errorf("Error hashing password: %w", err)
+	}
+	return string(hash), nil
+}
+
+func CheckPasswordHash(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
